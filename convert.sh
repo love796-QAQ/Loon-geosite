d885d6edcb1f42a19c421dac451df9c192567f61
love796-QAQ <80121684+love796-QAQ@users.noreply.github.com>
2024-10-31 00:03:50 +0800

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -10,15 +10,6 @@ git clone --branch rule-set "$repo_url" "$download_dir" || { echo "Failed to clo
 # 切换到下载目录
 cd "$download_dir" || { echo "Failed to enter directory"; exit 1; }
 
-# 检查当前目录是否为 Git 仓库
-if [ ! -d ".git" ]; then
-    echo "Not a git repository. Exiting."
-    exit 1
-fi
-
-# 输出当前工作目录
-echo "Current directory: $(pwd)"
-
 # 找到所有 .srs 文件
 srs_files=$(find . -name "*.srs")
 
@@ -29,12 +20,6 @@ for srs_file in $srs_files; do
     # 使用 sing-box 进行转换
     temp_output_file="temp_output.json"
     sing-box rule-set decompile --output "$temp_output_file" "$srs_file"
-
-    # 检查生成的 JSON 内容
-    if [ ! -f "$temp_output_file" ]; then
-        echo "Error: $temp_output_file not created. Skipping $srs_file."
-        continue
-    fi
 
     # 获取原文件名（不带后缀）
     filename="${srs_file%.srs}"
@@ -51,6 +36,9 @@ for srs_file in $srs_files; do
     rm "$temp_output_file"
     rm "$srs_file"  # 删除本地下载的 .srs 文件
 
+    # 切换到 rule-set 分支
+    git checkout rule-set
+
     # 设置 Git 用户身份
     git config user.name "love796-QAQ"
     git config user.email "wangshuo523@outlook.com"
@@ -60,9 +48,6 @@ for srs_file in $srs_files; do
 
     # 提交更改
     git commit -m "Update $final_output_file with domains and suffixes"
-
-    # 推送更改，强制覆盖
-    git push --force origin rule-set
 done
 
 # 删除除 .txt 文件外的所有文件
@@ -70,3 +55,6 @@ find . -type f ! -name "*.txt" -exec rm -f {} +
 
 # 删除空文件夹
 find . -type d -empty -exec rmdir {} +
+
+# 推送更改，强制覆盖
+git push --force origin rule-set
